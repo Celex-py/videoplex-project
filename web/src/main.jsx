71d0e199d59bba8a69ae63911cc8ac11dc7ff3ab
Celex-py <ugochukwuokeke343@gmail.com>
2026-09-16 12:12:50 +0100

    Add Live TV IPTV integration
    Add Free-TV IPTV channel browsing and HLS playback to Videoplex

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import { createPortal } from "react-dom";
 import App from "../App.jsx";
+import LiveTV from "../LiveTV.jsx";
+import "../LiveTV.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+function LiveTVMount() {
+  const [host, setHost] = useState(null);
+
+  useEffect(() => {
+    const findHost = () => {
+      const home = [...document.querySelectorAll('.sbi.on')].find(el => el.textContent?.includes('Home'));
+      const nextHost = home ? document.querySelector('.mn .pg') : null;
+      setHost(nextHost || null);
+    };
+
+    const observer = new MutationObserver(findHost);
+    observer.observe(document.body, { subtree: true, childList: true, attributes: true, attributeFilter: ['class'] });
+    const timer = window.setInterval(findHost, 500);
+    findHost();
+
+    return () => {
+      observer.disconnect();
+      window.clearInterval(timer);
+    };
+  }, []);
+
+  if (!host) return null;
+  return createPortal(<LiveTV />, host);
+}
+
+function Root() {
+  return (
+    <React.StrictMode>
+      <App />
+      <LiveTVMount />
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
